b0043e193b61676e64449117ea2cf4d2ce254d4f
marynanesterenko <marynanesterenko95@gmail.com>
2023-02-07 20:56:23 -0500

    Additional code added

--- src/test/java/commons/HandlingWindows.java
+++ src/test/java/commons/HandlingWindows.java
@@ -4,8 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -34,8 +36,40 @@ public class HandlingWindows extends CommonMethods{
                 driver.switchTo().window(window);
                 WebElement header = driver.findElement(By.tagName("h1"));
                 System.out.println(header.getText());
+                driver.close();
             }
         }
+
+        driver.switchTo().window(primaryWindow);
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void windowHandlingExample() throws InterruptedException{
+
+        WebDriver driver = getDriver();
+        driver.navigate().to("https://magento.softwaretestingboard.com/");
+
+        Actions actions = new Actions(driver);
+
+        String primaryWindow = driver.getWindowHandle();
+        WebElement shopYogaBtn = driver.findElement(By.id("//span[contains(text(),'Shop New Yoga')]"));
+
+        actions.keyDown(Keys.CONTROL).click(shopYogaBtn).keyUp(Keys.CONTROL).build().perform();
+
+        ArrayList <String> windowList = new ArrayList<>(driver.getWindowHandles());
+
+        for (String windowHandle : windowList){
+            if (!windowHandle.equals(primaryWindow)){
+                driver.switchTo().window(windowHandle);
+
+                if(driver.getCurrentUrl().contains("/collections/yoga-new.html")){
+                    driver.close();
+                }
+            }
+        }
+
+        Thread.sleep(3000);
     }
 
     @Before
